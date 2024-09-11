6a5340b7274e9f3cfee3a987302d4b9ff7cb4110
elias <eliasoliveiradapaz@hotmail.com>
2024-09-10 21:22:33 -0300

    add problem 1010

--- 1010/1010.c
+++ 1010/1010.c
@@ -2,13 +2,12 @@
  
 int main() {
 
-    float number1[2];
-    float number2[2];
+    double number1, number2, qty1, qty2, value1, value2;
 
-    scanf("%f %f %f %f %f %f", &number1[0], &number1[1], &number1[2], &number2[0], &number2[1], &number2[2]);
+    scanf("%lf %lf %lf %lf %lf %lf", &number1, &qty1, &value1, &number2,&qty2 ,&value2);
 
-    // double total = (number1[1] * number1[2]) + (number2[1] * number2[2]);
+    double total = (qty1 * value1) + (qty2 * value2);
 
-    // printf("VALOR A PAGAR: R$ %.2lf\n", total);
+    printf("VALOR A PAGAR: R$ %.2lf\n", total);
 
 }
