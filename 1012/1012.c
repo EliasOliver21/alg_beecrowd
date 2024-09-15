373014f51cccce3c410d9e1f0a4324d5ce57f580
elias <eliasoliveiradapaz@hotmail.com>
2024-09-14 21:14:13 -0300

    add alg 1012

--- 1012/1012.c
+++ 1012/1012.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <math.h>
  
 int main() {
  
-   
- 
+   double a, b , c, pi=3.14159;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    double triangle = (a * c)/2;
+
+    double circle = pi * pow(c,2);
+
+    double trapezoid = ((a + b) * c)/2;
+
+    double square = pow(b,2);
+
+    double retangle = a * b;
+
+    printf("TRIANGULO: %.3lf\nCIRCULO: %.3lf\nTRAPEZIO: %.3lf\nQUADRADO: %.3lf\nRETANGULO: %.3lf\n", triangle, circle, trapezoid, square, retangle);
+     
     return 0;
 }
